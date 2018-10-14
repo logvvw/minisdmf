e7c2bd94d39cf118e1fd2fb0d0502fb8e19565f2
logvvw <logvvw@gmail.com>
2018-10-14 13:29:01 +0800

    luanch class

---
+++ src/main/java/net/logvv/minisdmf/Application.java
@@ -0,0 +1,29 @@
+package net.logvv.minisdmf;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * main application
+ * @author logvvw
+ */
+@EnableAutoConfiguration
+@ImportResource("classpath:spring.xml")
+@EnableAsync
+@EnableScheduling
+public class Application
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Application.class, args);
+
+        LOGGER.info("Begin to run minisdmf ...");
+    }
+}
